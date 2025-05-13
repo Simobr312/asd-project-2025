b0501042f4f0598d5b44d9b9538bc14cae8aa9a9
Simobr312 <simobr312@gmail.com>
2025-05-13 23:25:32 +0200

    Setting the branch from my personal computer DELL XPS 15

---
+++ bifParser.cpp
@@ -0,0 +1,297 @@
+#include <bits/stdc++.h>
+
+//https://www.bnlearn.com/bnrepository/
+//http://www.cs.washington.edu/dm/vfml/appendixes/bif.htm
+
+class Token {
+    public:
+        enum Type {KEYWORD, WORD, SYMBOL, DECIMAL_LITERAL, FLOATING_POINT_LITERAL, END, TEXT, IGNORE};
+        Type type;
+        std::string value;
+
+        inline static const std::vector<std::pair<Type, std::regex>> Spec = {
+            {IGNORE,     std::regex(R"(^(\s+|,+|\|+|\"|/\*.*?\*/))")},
+            {KEYWORD,    std::regex(R"(^\b(network|variable|probability|property|type|discrete|default|table)\b)")},
+            {WORD, std::regex(R"(^[a-zA-Z_][a-zA-Z0-9_]*)")},
+            {SYMBOL,     std::regex(R"(^[{}()[\];=,`|])")},
+            {FLOATING_POINT_LITERAL,     std::regex(R"(^\d+\.(\d+)?)")},
+            {DECIMAL_LITERAL,     std::regex(R"(^\d+)")}
+        };
+
+        Token(Type type = END, std::string value = "")
+            : type(type), value(value) {};
+
+        friend std::ostream& operator<<(std::ostream& os, const Token& token) {
+            const std::string typeStr[7] = {
+                "KEYWORD", "WORD", "SYMBOL", "DEC_LIT", "FLOAT", "END", "TEXT"
+            };
+
+            os<<"type: "<< typeStr[token.type]<<" \t value: "<<token.value<<std::endl;
+            return os;
+        }
+};
+
+class Lexer {
+    private:
+        std::string source;
+        std::string::iterator cursor;
+        std::vector<Token> tokens_window;
+
+    public:
+        Lexer(std::ifstream& input) 
+            : source(std::istreambuf_iterator<char>(input), std::istreambuf_iterator<char>()),
+                cursor(source.begin()) {}
+
+        Token getNextToken() {
+            if (!tokens_window.empty()) {
+                Token current = tokens_window.front();
+                tokens_window.erase(tokens_window.begin());
+                return current;
+            }
+            Token current = lexNextToken();
+            while (current.type == Token::IGNORE) {
+                    current = lexNextToken();
+            }
+            return current;
+        }
+
+        Token peekNextToken(int n = 0) {
+            while (tokens_window.size() <= n) {
+                Token next = lexNextToken();
+                while (next.type == Token::IGNORE) {
+                    next = lexNextToken();
+                }
+                if (next.type != Token::END)
+                    tokens_window.push_back(next);
+            }
+            return tokens_window[n];
+        }
+
+    private:
+        Token lexNextToken() {         //I could simplify the structure by removing the window
+            std::smatch match_result;
+
+            if(cursor == source.end())
+                return Token(Token::END, "");
+            
+            std::string remaining = std::string(cursor, source.end());
+            for(auto [type, regex] : Token::Spec) {
+                std::regex_search(remaining, match_result, regex);
+
+                if(match_result.empty())
+                    continue;
+                
+                std::string value = match_result.str();
+                cursor += value.size();
+                return Token(type, value);
+            }
+
+            throw std::runtime_error("Unexpected token");
+        }
+};
+
+//
+// Abstract Syntax Tree
+//
+struct Variable {
+    std::string name;
+    std::vector<std::string> values;
+};
+
+struct Probability {
+    std::string variable;
+    std::vector<std::string> parents;
+    std::vector<std::vector<double>> table;
+};
+
+struct Network {
+    std::string name;
+    std::vector<Variable> variables;
+    std::vector<Probability> probabilities;
+};
+
+class Parser {
+    private:
+        Lexer lexer;
+        Token current;
+
+        Network network;
+
+        void advance() {
+            current = lexer.getNextToken();
+            //std::cout<<current<<std::endl;
+        }
+
+        Token expect(Token::Type expectedType, const std::string expectedValue = "") {
+            if(current.type != expectedType || (!std::empty(expectedValue) && current.value != expectedValue)) {
+                throw std::runtime_error("Expected " + expectedValue + ", got " + current.value);
+            }
+            Token token = current;
+            advance();
+            return token;
+        }
+
+    public:
+        Parser(std::ifstream& input)
+            :  lexer(input){
+                advance();
+        }
+
+        void parse() {
+            parseCompilationUnit();
+        }
+
+    private:
+
+        std::vector<double> parseFloatingPointList() {
+            std::vector<double> floatingPointList;
+            do {
+                floatingPointList.push_back(std::stod(expect(Token::FLOATING_POINT_LITERAL).value));
+            } while(current.value != ";");
+            expect(Token::SYMBOL, ";");
+            return floatingPointList;
+        }
+
+        void parseProababilityValuesList() {
+            if(current.value == "table") {
+                expect(Token::KEYWORD, "table");
+                return;
+            }
+            expect(Token::SYMBOL, "(");
+                do { //I'm skipping this data because untile now I don't need them
+                    advance();
+                    if(current.type == Token::END)
+                        break;
+                } while(current.value != ")");
+            expect(Token::SYMBOL, ")");
+        }
+
+        std::vector<std::vector<double>> parseProbabilityContent() {
+            expect(Token::SYMBOL, "{");
+
+            std::vector<std::vector<double>> table;
+
+            do {
+                parseProababilityValuesList();
+                table.push_back(parseFloatingPointList());
+            }while(current.value == "(");
+            
+            expect(Token::SYMBOL, "}");
+
+            return table;
+        }
+
+        std::pair<std::string, std::vector<std::string>> parseProabilityVariablesList() {
+            std::string variable;
+            std::vector<std::string> parents;
+            expect(Token::SYMBOL, "(");
+
+            variable = expect(Token::WORD).value;
+
+            while(current.value != ")"){
+                parents.push_back(expect(Token::WORD).value);
+            }
+
+            expect(Token::SYMBOL, ")");
+            return {variable, parents};
+        }
+
+        Probability parseProbabilityDeclaration() {
+            Probability probability;
+            expect(Token::KEYWORD, "probability");
+
+            auto [variable, parents] = parseProabilityVariablesList();
+            probability.variable = variable;
+            probability.parents = parents;
+            
+            std::vector<std::vector<double>> table = parseProbabilityContent();
+            probability.table = table;
+
+            return probability;
+        }
+
+        std::vector<std::string> parseVariableDiscrete() {
+            expect(Token::SYMBOL, "{");
+
+            expect(Token::KEYWORD, "type");
+            expect(Token::KEYWORD, "discrete");
+            expect(Token::SYMBOL, "[");
+            int n = std::stoi(expect(Token::DECIMAL_LITERAL).value);
+            std::vector<std::string> values(n);
+            expect(Token::SYMBOL, "]");
+            expect(Token::SYMBOL, "{");
+            
+            for (int i = 0; i < n; ++i) {
+                std::string value = expect(Token::WORD).value;
+                values[i] = value;
+            }
+
+            expect(Token::SYMBOL, "}");
+            expect(Token::SYMBOL, ";");
+
+            expect(Token::SYMBOL, "}");
+            return values;
+        }
+
+        Variable parseVariableDeclaration() {
+            Variable variable;
+            expect(Token::KEYWORD, "variable");
+
+            Token name = expect(Token::WORD);
+            variable.name = name.value;
+
+            //I'm ignoring inner properties for now, and so the parseVariableContent() production
+            variable.values = parseVariableDiscrete();
+            return variable;
+        }
+
+        void parseNetworkDeclaration() {
+            expect(Token::KEYWORD, "network");
+
+            Token name = expect(Token::WORD);
+            network.name = name.value;
+
+            // I'm  ignoring inner properties for now, so I'm not implementing the NetworkContent production
+            while (current.value != "}") {
+                if(current.type == Token::END)
+                    throw std::runtime_error("Unexpected end of input in network declaration");
+                advance();
+            }
+
+            expect(Token::SYMBOL, "}");
+        }
+
+        void parseCompilationUnit() {
+            parseNetworkDeclaration();
+
+            while(true) {
+                if(current.type == Token::END)
+                    break;
+
+                if(current.type == Token::KEYWORD) {
+                    if(current.value == "variable") {
+                        Variable variable = parseVariableDeclaration();
+                        network.variables.push_back(variable);
+                    }
+                    else if(current.value == "probability") {
+                        Probability probability = parseProbabilityDeclaration();
+                        network.probabilities.push_back(probability);
+                    }
+                } else {
+                    throw std::runtime_error("Expected 'variable' or 'probability', got " + current.value);
+                }
+            }
+            
+        }
+};
+
+int main() {
+
+    std::string filename = "BIF/asia.bif";
+    std::ifstream input(filename);    
+    Parser parser(input);
+
+    parser.parse();
+
+    return 0;
+}
