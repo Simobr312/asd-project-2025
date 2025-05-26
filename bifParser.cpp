b3f86233d50c9de1af5586abf3f514516182ad44
Simobr312 <simobr312@gmail.com>
2025-05-26 23:48:48 +0200

    Changed the Matcher class and his children to be only a class with a function paramater. Now to code has way less random classes.

--- bifParser.cpp
+++ bifParser.cpp
@@ -3,175 +3,6 @@
 //https://www.bnlearn.com/bnrepository/
 //http://www.cs.washington.edu/dm/vfml/appendixes/bif.htm
 
-class Matcher {
-    protected:
-        static bool isADigit(const char c) {
-            return c >= '0' && c <= '9';
-        }
-
-        static bool isAlpha(const char c) {
-            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-        }
-
-        static bool isAlphaNumeric(const char c) { //The underscore is not allowed by the documentation, but it is needed for the data.
-            return isADigit(c) || isAlpha(c) || c == '_';
-        }
-
-    public:
-        Matcher() = default;
-        ~Matcher() = default;
-
-    virtual std::pair<bool, std::string::iterator> try_match(const std::string::iterator cursor, const std::string::iterator end) = 0;
-};
-
-class IgnoreMatcher : public Matcher {
-    private:
-        bool isIgnoredSymbols(const char c) {
-            return c == ' ' || c == '|' || c == ',' || c == '"' || c == '\n';
-        }
-
-        bool matchCommentStart(const std::string::iterator it, const std::string::iterator end) {
-            return it != end && *it == '/' && (it + 1) != end && *(it + 1) == '*';
-        }
-
-        bool matchCommentEnd(const std::string::iterator it, const std::string::iterator end) {
-            return it != end && *it == '*' && (it + 1) != end && *(it + 1) == '/';
-        }
-
-    public:
-        std::pair<bool, std::string::iterator> try_match(const std::string::iterator cursor, const std::string::iterator end) override {
-            auto it = cursor;
-        
-            while(it != end && isIgnoredSymbols(*it)) ++it;
-            
-            if (matchCommentStart(it, end)) {
-                it += 2; // skip /*
-                while(it != end) {
-                    if (matchCommentEnd(it, end)) {
-                         it += 2; // skip */
-                        break;
-                    }
-                    ++it;
-                }
-            }
-            
-            return {(it == cursor) ? false : true, it};
-        }
-};
-
-class KeywordMatcher : public Matcher { //R"(^\b(network|variable|probability|property|type|discrete|default|table)\b)" 
-    private:
-        const std::vector<std::string> keywords;
-    public:
-        KeywordMatcher(const std::vector<std::string>& keywords) 
-            : keywords(keywords) {}
-
-        bool theWordEnds(const std::string::iterator it) {
-            return !isAlphaNumeric(*it);
-        }
-
-        std::pair<bool, std::string::iterator> try_match(const std::string::iterator cursor, const std::string::iterator end) override {
-            auto it = cursor;
-            for(auto keyword : keywords) {
-                it = cursor + keyword.size();
-                if(it <= end && std::string(cursor, it) == keyword && theWordEnds(it))
-                        return {true, it};
-            }
-            return {false, it};
-        }
-};
-
-//The regular expression of the WORD token from the documentation I found is R"(^[a-zA-Z_][a-zA-Z0-9_]*)",
-//but for the seek of the project, in the data given by the professor it is useful to consider WORD even strings
-//which do not start with an alphabetical character.
-class WordMatcher : public Matcher { 
-    public:
-        std::pair<bool, std::string::iterator> try_match(const std::string::iterator cursor, const std::string::iterator end) override {
-            auto it = cursor;
-
-            while (it != end && isAlphaNumeric(*it))  // letters or digits
-                ++it;
-
-            return {true, it};
-        }
-};
-
-class CharSetMatcher : public Matcher { //  R"(^[{}()[\];=,`|])"
-    private:
-        const std::string ALLOWED;
-    public:
-        CharSetMatcher(const std::string& allowed)
-            : ALLOWED(allowed) {}
-
-        bool isAllowed(const char c) {
-            return ALLOWED.find(c) != std::string::npos;
-        }
-
-        std::pair<bool, std::string::iterator> try_match(const std::string::iterator cursor, const std::string::iterator end) override {
-            auto it = cursor;
-
-            if(it != end && isAllowed(*it)) {
-                ++it;
-                return {true, it};
-            }
-
-            return {false, it};
-        }
-};
-
-class DecimalMatcher : public Matcher { //R"(^\d+(?!\.|[eE]))"
-    public:
-        std::pair<bool, std::string::iterator> try_match(const std::string::iterator cursor, const std::string::iterator end) override {
-            auto it = cursor;
-
-            if (!isADigit(*it))
-                return {false, it};
-
-            while (it != end && isADigit(*it))
-                ++it;
-
-            if(!isAlphaNumeric(*it)) //making sure is not the start of a word.
-                return {true, it};
-            return {false, it};
-        }
-};
-
-class FloatMatcher : public Matcher { //R"(^((\d+\.\d*|\.\d+|\d+)([eE][+-]?\d+)?))"
-    public:
-        std::pair<bool, std::string::iterator> try_match(const std::string::iterator cursor, const std::string::iterator end) override {
-            auto it = cursor;
-
-            if (!isADigit(*it))
-                return {false, it};
-
-            while (it != end && isADigit(*it))
-                ++it;
-
-            if (it == end || *it != '.')  // must have decimal point for float
-                return {false, it};
-
-            ++it; // '.'
-
-            if (it == end || !isADigit(*it))
-                return {false, it};
-
-            while (it != end && isADigit(*it))
-                ++it;
-            
-            if(it != end && *it == 'e') {
-                ++it;
-                if(it != end && (*it == '+' || *it == '-'))
-                    ++it;
-            }
-
-            while (it != end && isADigit(*it))
-                ++it;
-
-            return {true, it};
-        }
-};
-
-
 class Token {
     public:
         enum Type {KEYWORD, WORD, SYMBOL, DECIMAL_LITERAL, FLOATING_POINT_LITERAL, END, TEXT, IGNORE};
@@ -189,48 +20,194 @@ class Token {
             os<<"type: "<< typeStr[token.type]<<" \t value: "<<token.value<<std::endl;
             return os;
         }
+
+        inline static const std::vector<std::string> KEYWORDS = {
+            "network", "variable", "probability", "table", "type", "discrete", "default", "property"
+        };
+
+        inline static const std::string SYMBOLS = "[{}()[];=,`|]";
 };
+
+typedef std::pair<bool, std::string::iterator> match_result_t;
+typedef std::function<match_result_t(std::string::iterator, std::string::iterator)> matcher_function_t;
+class Matcher {
+    protected:
+        static bool isADigit(const char c) {
+            return c >= '0' && c <= '9';
+        }
+
+        static bool isAlpha(const char c) {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+        }
+
+        static bool isAlphaNumeric(const char c) { //The underscore is not allowed by the documentation, but it is needed for the data.
+            return isADigit(c) || isAlpha(c) || c == '_';
+        }
+
+        static bool isIgnoredSymbols(const char c) {
+            return c == ' ' || c == '|' || c == ',' || c == '"' || c == '\n';
+        }
+
+        static bool matchCommentStart(const std::string::iterator it, const std::string::iterator end) {
+            return it != end && *it == '/' && (it + 1) != end && *(it + 1) == '*';
+        }
+
+        static bool matchCommentEnd(const std::string::iterator it, const std::string::iterator end) {
+            return it != end && *it == '*' && (it + 1) != end && *(it + 1) == '/';
+        }
+
+        static bool theWordEnds(const std::string::iterator it) {
+            return !isAlphaNumeric(*it);
+        }
+
+    public:
+        Token::Type type;
+        matcher_function_t matchFunc;
+
+        Matcher(Token::Type type, matcher_function_t func)
+            : type(type), matchFunc(func) {}
+
+        match_result_t try_match(std::string::iterator cursor, std::string::iterator end) {
+            return matchFunc(cursor, end);
+        }
+
+        static match_result_t ignoreMatcherFunc(const std::string::iterator cursor, const std::string::iterator end) {
+            auto it = cursor;
+        
+            while(it != end && isIgnoredSymbols(*it)) ++it;
+            
+            if (matchCommentStart(it, end)) {
+                it += 2; // skip /*
+                while(it != end) {
+                    if (matchCommentEnd(it, end)) {
+                         it += 2; // skip */
+                        break;
+                    }
+                    ++it;
+                }
+            }
+            
+            return {(it == cursor) ? false : true, it};
+        }
+
+        //R"(^\b(network|variable|probability|property|type|discrete|default|table)\b)"
+        static match_result_t keyWordMatcherFunc(const std::string::iterator cursor, const std::string::iterator end) {
+            auto it = cursor;
+            for(auto keyword : Token::KEYWORDS) {
+                it = cursor + keyword.size();
+                if(it <= end && std::string(cursor, it) == keyword && theWordEnds(it))
+                        return {true, it};
+            }
+            return {false, it};
+        }
+
+
+        //The regular expression of the WORD token from the documentation I found is R"(^[a-zA-Z_][a-zA-Z0-9_]*)",
+        //but for the seek of the project, in the data given by the professor it is useful to consider WORD even strings
+        //which do not start with an alphabetical character.
+        static match_result_t wordMatcherFunc(const std::string::iterator cursor, const std::string::iterator end) {
+            auto it = cursor;
+
+            while (it != end && isAlphaNumeric(*it))  // letters or digits
+                ++it;
+
+            return {true, it};
+        }
+
+        //  R"(^[{}()[\];=,`|])"
+        static match_result_t symbolMatcherFunc(const std::string::iterator cursor, const std::string::iterator end) {
+            auto it = cursor;
+
+            if(it != end && Token::SYMBOLS.find(*it) != std::string::npos) {
+                ++it;
+                return {true, it};
+            }
+
+            return {false, it};
+        }   
+
+        static match_result_t decimalMatcherFunc(const std::string::iterator cursor, const std::string::iterator end) {
+            auto it = cursor;
+
+            if (!isADigit(*it))
+                return {false, it};
+
+            while (it != end && isADigit(*it))
+                ++it;
+
+            if(!isAlphaNumeric(*it)) //making sure is not the start of a word.
+                return {true, it};
+            return {false, it};
+        }
+
+
+        //R"(^\d+(?!\.|[eE]))"
+        static match_result_t floatMatcherFunc(const std::string::iterator cursor, const std::string::iterator end) {
+            auto it = cursor;
+
+            bool seenDot = false;
+            bool seenExp = false;
+
+            if (!isADigit(*it))
+                return {false, it};
+
+            while (it != end && isADigit(*it)) ++it;
+
+            if (it != end && *it == '.') {
+                ++it;
+                seenDot = true;
+                while (it != end && isADigit(*it)) ++it;
+            }
+
+            if (it != end && (*it == 'e' || *it == 'E')) {
+                ++it;
+                seenExp = true;
+                if (it != end && (*it == '+' || *it == '-')) ++it;
+                while (it != end && isADigit(*it)) ++it;
+            }
+
+            if (seenDot || seenExp)
+                return {true, it};
+            else
+                return {false, cursor};
+        }
+};  
+
 
 class Lexer {
     private:
         std::string source;
         std::string::iterator cursor;
 
-        std::unique_ptr<Matcher> ignoreMatcher;
-        std::vector<std::pair<Token::Type, std::unique_ptr<Matcher>>> matchers;
-
-        inline static const std::vector<std::string> KEYWORDS = {
-            "network", "variable", "probability", "table", "type", "discrete", "default", "property"
-        };
+        Matcher ignoreMatcher;
+        std::vector<std::pair<Token::Type, Matcher>> matchers;
 
     public:
         Lexer(std::ifstream& input) 
         : source(std::istreambuf_iterator<char>(input), std::istreambuf_iterator<char>()),
-          cursor(source.begin()) {
-
-            ignoreMatcher = std::make_unique<IgnoreMatcher>();
-
-            matchers.push_back({Token::SYMBOL, std::make_unique<CharSetMatcher>("[{}()[];=,`|]")});
-            matchers.push_back({Token::FLOATING_POINT_LITERAL, std::make_unique<FloatMatcher>()}); 
-            matchers.push_back({Token::DECIMAL_LITERAL, std::make_unique<DecimalMatcher>()}); //and so decimal after floating
-            matchers.push_back({Token::KEYWORD, std::make_unique<KeywordMatcher>(KEYWORDS)});
-            //Only after I matched DECIMAL LITERAL and KEYWORD I can try WORD, because WORD contains the others.
-            matchers.push_back({Token::WORD, std::make_unique<WordMatcher>()});
-        }
+          cursor(source.begin()),
+           ignoreMatcher(Token::IGNORE, Matcher::ignoreMatcherFunc),
+           matchers({ //The order is foundamental, because some regular expression cointains others
+                {Token::SYMBOL, Matcher(Token::SYMBOL, Matcher::symbolMatcherFunc)},
+                {Token::FLOATING_POINT_LITERAL,  Matcher(Token::FLOATING_POINT_LITERAL, Matcher::floatMatcherFunc)},
+                {Token::DECIMAL_LITERAL,        Matcher(Token::DECIMAL_LITERAL, Matcher::decimalMatcherFunc)},
+                {Token::KEYWORD,Matcher(Token::KEYWORD, Matcher::keyWordMatcherFunc)},
+                {Token::WORD,   Matcher(Token::WORD, Matcher::wordMatcherFunc)}
+            }) {
+            }
 
         Token getNextToken() {
             if(cursor == source.end())
                 return Token(Token::END, "");
 
-            auto [match_res, new_cursor] = ignoreMatcher->try_match(cursor, source.end());
-
+            auto [match_res, new_cursor] = ignoreMatcher.try_match(cursor, source.end());
             if(match_res == true) {
                 cursor = new_cursor;
                 return getNextToken();
             }
 
             for(auto& [type, matcher] : matchers) {
-                auto [match_res, new_cursor] = matcher->try_match(cursor, source.end());
+                auto [match_res, new_cursor] = matcher.try_match(cursor, source.end());
                 if(match_res == true) {
                     std::string value = std::string(cursor, new_cursor);
                     cursor = new_cursor;
@@ -305,7 +282,7 @@ class Parser {
             return floatingPointList;
         }
 
-        void parseProababilityValuesList() {
+        void parseProbabilityValuesList() {
             if(current.value == "table") {
                 expect(Token::KEYWORD, "table");
                 return;
@@ -325,7 +302,7 @@ class Parser {
             std::vector<std::vector<double>> table;
 
             do {
-                parseProababilityValuesList();
+                parseProbabilityValuesList();
                 table.push_back(parseFloatingPointList());
             }while(current.value == "(");
             
@@ -374,6 +351,7 @@ class Parser {
             expect(Token::SYMBOL, "]");
             expect(Token::SYMBOL, "{");
             
+            values.reserve(n);
             for (int i = 0; i < n; ++i) {
                 std::string value;
 
@@ -430,12 +408,12 @@ class Parser {
                 if(current.type == Token::KEYWORD) {
                     if(current.value == "variable") {
                         Variable variable = parseVariableDeclaration();
-                        std::cout<<"variable: "<<variable.name<<std::endl;
+                        //std::cout<<"variable: "<<variable.name<<std::endl;
                         network.variables.push_back(variable);
                     }
                     else if(current.value == "probability") {
                         Probability probability = parseProbabilityDeclaration();
-                        std::cout<<"probability: "<<probability.variable<<std::endl;
+                        //std::cout<<"probability: "<<probability.variable<<std::endl;
                         network.probabilities.push_back(probability);
                     }
                 } else
